8784696124e55c8fe3c1f9dc53e5af65625d29ff
JasonW <silentred3@gmail.com>
2017-06-14 14:43:37 +0800

    add comments

--- config/config.go
+++ config/config.go
@@ -15,22 +15,23 @@ const (
 	RotateBySize = "size"
 )
 
+// AppConfig for application
 type AppConfig struct {
 	Name  string
 	Mode  string
 	Port  int
-	Sess  SessionConfig
 	Log   LogConfig
 	Mysql MysqlConfig
 	Redis RedisConfig
 }
 
-type SessionConfig struct {
+type sessionConfig struct {
 	Providor  string
 	StorePath string
 	Enable    bool
 }
 
+// LogConfig for logger
 type LogConfig struct {
 	Name         string
 	Providor     string
@@ -41,12 +42,14 @@ type LogConfig struct {
 	RotateEnable bool
 }
 
+// MysqlConfig for MySQL
 type MysqlConfig struct {
 	Instances []MysqlInstance
 	InitMySQL bool
 	Ping      bool
 }
 
+// MysqlInstance represents a single instance of mysql server
 type MysqlInstance struct {
 	Name     string `json:"name"`
 	Host     string `json:"host"`
@@ -61,12 +64,14 @@ func (inst MysqlInstance) String() string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8", inst.User, inst.Pwd, inst.Host, inst.Port, inst.Db)
 }
 
+// RedisConfig for redis
 type RedisConfig struct {
 	InitRedis bool
 	Ping      bool
 	RedisInstance
 }
 
+// RedisInstance represents a single instance of redis server
 type RedisInstance struct {
 	Name string `json:"name"`
 	Host string `json:"host"`
@@ -75,6 +80,7 @@ type RedisInstance struct {
 	Db   int    `json:"database"`
 }
 
+// Address returns the address of redis server
 func (inst RedisInstance) Address() string {
 	return fmt.Sprintf("%s:%d", inst.Host, inst.Port)
 }
