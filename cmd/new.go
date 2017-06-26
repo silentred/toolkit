b1789efddb5ecefd51105830f2b199fc2283d5ee
JasonW <silentred3@gmail.com>
2017-06-26 10:42:49 +0800

    fix cd path

--- cmd/new.go
+++ cmd/new.go
@@ -189,7 +189,7 @@ func RunNew(path, appName string) {
 	makeFiles(gopath, path, appName)
 
 	color.Yellow("# Step2: run following commands to start the app")
-	color.Blue("cd $GOPATH/%s/%s", path, appName)
+	color.Blue("cd $GOPATH/src/%s/%s", path, appName)
 	color.Blue("git init && git add * && git commit -m \"init commit\" ")
 	color.Blue("go get github.com/labstack/echo")
 	color.Blue("make build && ./%s", appName)
